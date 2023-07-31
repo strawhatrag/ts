e2aaf0cc3cdae9b7a2a6d8b6a3ae248939138a16
strawhatrag <sreeragbharathan791@gmail.com>
2023-07-31 15:36:24 +0530

    array over

---
+++ arrays.ts
@@ -0,0 +1,11 @@
+const users: string[] = [];
+users.push('tony')
+
+
+// identical
+const anotherUsers: Array<string> = [];
+anotherUsers.push('stark')
+
+
+//Multi dimensional
+const chart: string[][] = [['x', 'y', 'z'], ['x', 'y', 'z'], ['x', 'y', 'z']]
