dfc7799cb00e72d0010b73bd9d2ce9d658aec0da
strawhatrag <sreeragbharathan791@gmail.com>
2023-08-05 13:48:54 +0530

    params shorthand

--- classes/src/index.js
+++ classes/src/index.js
@@ -1,56 +1,24 @@
-class Player {
-  static ablilites = "psychic abilities"; // only exists for the class , not for instance
-
-  level = 80; // properties don't need let, const etc...
-  #feelings = "lonely"; // #var is private cannot access it outside
-  constructor(first, last) {
-    this.first = first;
-    this.last = last;
-  }
-
-  incident() {
-    this.level += 10; // this is needed to access properties
-    this.#feelings = "angry";
-  }
-
-  // getter
-  get fullname() {
-    return `${this.first} ${this.last}`;
-  }
-
-  // setter
-  set level(newLevel) {
-    if (newLevel < 0 || newLevel > 100) {
-      throw new Error("level range invalid");
+// class Player {
+//   public readonly first: string;   // readonly is optional okay!
+//   // public, private modifier  means readbility outside the fn
+//   public readonly last: string;    // public  is optional cuz everything is public by default 
+//   private score: number = 0;
+//   constructor(first: string, last: string) {
+//     this.first = first;  // you need to instantiate the type before like above 
+//     this.last = last;
+//   }
+//   private secretMethod(): void {  // only accessible inside the class
+//     console.log("secret method!")
+//   }
+// }
+var Player = /** @class */ (function () {
+    function Player(first, last) {
+        this.first = first;
+        this.last = last;
+        this.score = 0;
+        this.first = first;
+        this.last = last;
     }
-    this.level = newLevel;
-  }
-}
-
-//inhertitance
-class AdminPlayer extends Player {
-  // super() - to override the default constructor but pass the default params
-  constructor(first, last, powers) {
-    super(first, last); // inside the constructor
-    this.powers = powers;
-  }
-
-  isAdmin = true;
-}
-
-const eminem = new AdminPlayer("slim", "shady", [
-  "super vision",
-  "super human Strength",
-]);
-
-const mob = new Player("mob", "pyscho");
-console.log(mob.level); // level is not private
-mob.incident();
-console.log(mob.level);
-
-//getter
-console.log(mob.fullname); //not fullname() since getter
-
-// setter
-mob.level = 50; // set you can assign level like a var
-console.log(mob.level);
+    return Player;
+}());
+var mob = new Player("mob", "psycho");
---
+++ classes/src/index.ts
@@ -0,0 +1,32 @@
+// class Player {
+//   public readonly first: string;   // readonly is optional okay!
+//   // public, private modifier  means readbility outside the fn
+//   public readonly last: string;    // public  is optional cuz everything is public by default 
+
+//   private score: number = 0;
+
+
+//   constructor(first: string, last: string) {
+//     this.first = first;  // you need to instantiate the type before like above 
+//     this.last = last;
+
+//   }
+
+//   private secretMethod(): void {  // only accessible inside the class
+//     console.log("secret method!")
+//   }
+
+// }
+
+class Player {
+  private score: number = 0;
+  constructor(public first: string, public last: string) {
+    this.first = first;
+    this.last = last;
+
+  }
+
+
+}
+
+const mob = new Player("mob", "psycho")
