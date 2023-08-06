77e63019c46d9ba74a8bffa5289d6d9fef30257e
strawhatrag <sreeragbharathan791@gmail.com>
2023-08-06 22:26:27 +0530

    genric end

--- generic/dist/index.js
+++ generic/dist/index.js
@@ -1,3 +1,16 @@
 let inputEl = document.querySelector("#username");
 console.dir(inputEl);
 inputEl.value = "Hacked!";
+function getRandomElement(list) {
+    const randIdx = Math.floor(Math.random() * list.length);
+    return list[randIdx];
+}
+getRandomElement(['a', 'b', 'c']);
+getRandomElement([5, 10, 20, 30, 40, 50]);
+getRandomElement(['a', 'b', 'c']);
+getRandomElement([5, 10, 20, 30, 40, 50]);
+console.log('');
+function merge(object1, object2) {
+    return Object.assign(Object.assign({}, object1), object2);
+}
+const comboObj = merge({ name: 'rag' }, { skills: ['js', 'ts', 'fullstack'] });
